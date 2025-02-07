2661eb0f95adca8a61546a10b4fd7d3e1dfc788a
hashirama <cosies-fades-0@icloud.com>
2025-02-08 02:41:21 +0300

    start working on sensor_widget

---
+++ main_window/sensors_page/sensors_creator/sensor_widget/sensor_widget.cpp
@@ -0,0 +1,17 @@
+#include "sensors_creator.hpp"
+
+#include <QButtonGroup>
+#include <QComboBox>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QPushButton>
+#include <QRadioButton>
+#include <QSplitter>
+#include <QVBoxLayout>
+#include <iostream>
+
+sensors_creator::sensors_creator(QWidget* parent)
+	: QDialog(parent)
+{
+	
+}
---
+++ main_window/sensors_page/sensors_creator/sensor_widget/sensor_widget.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+
+class sensor_widget : public QDialog
+{
+	Q_OBJECT
+
+public:
+	sensor_widget(QWidget* parent = 0);
+	~sensor_widget() {};
+
+	
+private:
+	;
+};
