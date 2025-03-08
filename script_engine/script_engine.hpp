ce0d125d1401c6db83692e02159c941ed2e14d92
hashirama <cosies-fades-0@icloud.com>
2025-03-08 23:01:02 +0300

    add definition to script_engine

--- script_engine/script_engine.hpp
+++ script_engine/script_engine.hpp
@@ -1,18 +1,20 @@
 #define SOL_ALL_SAFETIES_ON 1
 #include <sol/sol.hpp>
 
+
 class script_engine
 {
 public:
 	script_engine();
-	script_engine( script_engine& se ) = delete;
+	script_engine( const script_engine& se )			 = delete;
+	script_engine& operator= ( const script_engine& se ) = delete;
 
-	void		add_object( const std::string& name,
-							sol::table		   object ); //	or register_object();
-	void		remove_object();
-	void		do_code_globally( std::string script );
-	void		do_code_separately( std::string script );
-	std::string _get_stack_dump();
+	void		   add_object( const std::string& name, sol::table object );
+	void		   remove_object(const std::string& name);
+	void		   do_code_globally( std::string script );
+	void		   do_code_separately( std::string script );
+	std::string	   get_stack_dump();
 
 private:
+	sol::state _state;
 };
